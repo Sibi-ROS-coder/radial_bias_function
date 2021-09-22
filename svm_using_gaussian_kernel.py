a7cf82fcaf6e131bc6c7638be55e655469ea293c
Sibi-Ros-Coder <sibi001@e.ntu.edu.sg>
2021-09-22 22:38:40 +0800

    svm code added

---
+++ svm_using_gaussian_kernel.py
@@ -0,0 +1,40 @@
+from enum import auto
+import numpy as np
+from numpy.core.numeric import correlate
+###import the scipy for loading .mat matlab data
+from scipy.io import loadmat
+### loading the data in same name variable
+data_test_tmp = loadmat('data_test.mat')
+data_train_tmp = loadmat('data_train.mat')
+label_train_tmp = loadmat('label_train.mat')
+#### printing the type and size of the variables
+print("data_test ",type(data_test_tmp),"_",len(data_test_tmp))
+print("data_train ",type(data_train_tmp),"_",len(data_train_tmp))
+print("label_train ",type(label_train_tmp),"_",len(label_train_tmp))
+###### converting the class dictionary to numpy array
+data_test = data_test_tmp["data_test"]
+data_train = data_train_tmp["data_train"]
+label_train = label_train_tmp["label_train"]
+data_test_row,data_test_column = data_test.shape
+data_train_row,data_train_column = data_train.shape
+label_train_row,label_train_column = data_test.shape
+print("data_test ",type(data_test),"_",data_test_row,data_test_column)
+
+print("data_train ",type(data_train),"_",data_train_row,data_train_column)
+print("label_train ",type(label_train),"_",label_train_row,label_train_column)
+
+X_train = data_train
+y_train = label_train
+X_test = data_train
+y_test = label_train
+from sklearn.svm import SVC
+svclassifier = SVC(kernel='rbf')
+svclassifier.fit(X_train, y_train)
+#To use Gaussian kernel, you have to specify 'rbf' as value for the Kernel parameter of the SVC class.
+
+#Prediction and Evaluation
+y_pred = svclassifier.predict(X_test)
+from sklearn.metrics import classification_report, confusion_matrix
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+#The output of the Kernel SVM with Gaussian kernel looks like this:
