788b4d2dbebcf8c049e2a6d5ce4817bf0c7fa640
Sibi-Ros-Coder <sibi001@e.ntu.edu.sg>
2021-09-25 19:36:42 +0800

    added separate file for both rbf and svm and creted test in rbf

--- neural_network_assignment.py
+++ neural_network_assignment.py
@@ -1,8 +1,6 @@
 import numpy as np
-from numpy.core.numeric import correlate
 ###import the scipy for loading .mat matlab data
 from scipy.io import loadmat
-
 ### loading the data in same name variable
 data_test_tmp = loadmat('data_test.mat')
 data_train_tmp = loadmat('data_train.mat')
@@ -19,142 +17,81 @@ data_test_row,data_test_column = data_test.shape
 data_train_row,data_train_column = data_train.shape
 label_train_row,label_train_column = data_test.shape
 print("data_test ",type(data_test),"_",data_test_row,data_test_column)
-
 print("data_train ",type(data_train),"_",data_train_row,data_train_column)
 print("label_train ",type(label_train),"_",label_train_row,label_train_column)
-##############
-# from sklearn.gaussian_process import GaussianProcessClassifier
-# from sklearn.gaussian_process.kernels import RBF
-# X = data_train
-# y = label_train[:,0]
-# kernel = 1.0 * RBF(1.0)
-# gpc = GaussianProcessClassifier(kernel=kernel,
-#         random_state=0).fit(X, y)
-# gpc.score(X, y)
-
-# gpc.predict_proba(X[:2,:])
-# print(gpc.score(X, y))
-# print(gpc.predict_proba(X[:2,:]))
-####################
-# class_1 = np.zeros([1,33])
-# class_2 = np.zeros([1,33])
-# num_class1 = 0
-# num_class2 = 0
-
-# i = 0
-# for i in range(len(data_train)):
-#     if(label_train[i,0] == 1):
-#         class_1[0,:] += data_train[i,:]
-#         num_class1 += 1
-#     else:
-#         class_2[0,:] += data_train[i,:]
-#         num_class2 += 1
-# print("Class_1",num_class1)
-# print("Class_2",num_class2)
-# central_neuron_1 = class_1/num_class1
-# central_neuron_2 = class_2/num_class2
-# print("central neuraon",central_neuron_1.shape)
+#########################################################
 import math
 def radial_gaussian_function(distance_in_neurons,sigma):
     g_of_d = math.exp((-1.0*distance_in_neurons*distance_in_neurons)/(2*sigma*sigma))
     return g_of_d
-# phi = np.zeros([330,2])
-
-# for j in range(len(data_train)):
-#     distance_in_neurons_1 = np.linalg.norm(data_train[j] - central_neuron_1)
-#     distance_in_neurons_2 = np.linalg.norm(data_train[j] - central_neuron_2)
-#     phi[j,0] = radial_gaussian_function(distance_in_neurons_1,0.707)
-#     phi[j,1] = radial_gaussian_function(distance_in_neurons_2,0.707)
-    
-# print("PHI",phi.shape)
-# weights = np.matmul(np.linalg.pinv([phi]),label_train)
-# # print("weights",weights)
-# f_of_x = np.matmul(phi,weights)
-# # print("f_of_x",f_of_x[0,:,0])
-
-# ijk = 0
-# correct = 0
-# for ijk in range(len(data_train)):
-#     # tmp1= round(f_of_x[0,ijk,0] - label_train[ijk,0])
-#     if(f_of_x[0,ijk,0] < 0 ):
-#         tmp1 = -1
-#     else:
-#         tmp1 = 1
-#     # print(f_of_x[0,ijk,0] - label_train[ijk,0])
-#     if ((tmp1 == -1 and label_train[ijk,0] ==-1 ) or ((tmp1 == 0 or tmp1 == 1 ) and label_train[ijk,0] == 1)):
-#         correct +=1
-# print("Acccuracy",100*(correct/len(data_train)))
-# print("wrong",len(data_train)-correct)
-
-from scipy.spatial import distance
 import random
+
 def weight_calculation(train_dataset,train_label_dataset,number_of_centres):
-    i_1, i_2,i_3 = 0 ,0 , 0
-    central_neurons = np.zeros([number_of_centres,data_train_column])
+    i_1, i_2,i_3 = 0 ,0 ,0
+    if (prechoose==0):
+        central_neurons = np.zeros([number_of_centres,data_train_column])
+    else:
+        number_of_centres = 20
+        central_neurons = np.zeros([number_of_centres,data_train_column])    
     distance_in_neurons = np.zeros([len(train_dataset),number_of_centres])
     PHI_1 = np.zeros([len(train_dataset),number_of_centres])
     for i_1 in range(number_of_centres):
-        central_neuron_id = round(random.random() *len(data_train))
-        # print("randomly chose the neurons",central_neuron_id)
-        central_neurons[i_1,:]  = train_dataset[central_neuron_id,:]
-    # abc = distance.euclidean(train_dataset,central_neurons)
-    
-    # print("centrL nEURONS",central_neurons)
+        if (prechoose == 0):
+            central_neuron_id = round(random.random() *len(data_train))
+            # print("randomly chose the neurons",central_neuron_id)
+            central_neurons[i_1,:]  = train_dataset[central_neuron_id,:]
+        else:
+            random_central_neruon =[141, 4, 329, 147, 318, 133, 185, 90, 47, 306, 48, 60, 12, 124, 41, 213, 99, 21, 308, 325]
+            number_of_centres = 20
+            central_neurons[i_1,:]  = train_dataset[random_central_neruon[i_1],:]
+
     d_max = 0
     for i_2 in range(len(train_dataset)):
         for i_3 in range(number_of_centres):
-            # distance_in_neurons[i_2,i_3] = distance.euclidean(train_dataset[i_2,:],central_neurons[i_3,:])
             d_distance = np.linalg.norm(train_dataset[i_2,:] - central_neurons[i_3,:])
             if (d_max<=d_distance):
                 d_max = d_distance
+            else:
+                d_max = d_max
             
             sigma = d_max/(number_of_centres**0.5)
             PHI_1[i_2,i_3] = radial_gaussian_function(d_distance,sigma)
-    # print("Max_distace",d_max)
-    # print("distance",distance_in_neurons)
     weights_final = np.matmul(np.linalg.pinv([PHI_1]),train_label_dataset)
-    print("WEIGHTS",weights_final.shape)
-    print("phi",PHI_1.shape)
-    # print("weights",weights_final)
+    
+    print("TRAIN WEIGHTS",weights_final[0,:,:].shape)
+    print("TRAIN_PHI",PHI_1.shape)
     f_of_x_1 = np.matmul(PHI_1,weights_final)
-    print("f_of_x",f_of_x_1.shape)
+    print("TRAIN f_of_x",f_of_x_1[0,:,:].shape)
 
     ijk_1 = 0
     correct_1 = 0
     for ijk_1 in range(len(data_train)):
-        # tmp1= round(f_of_x[0,ijk,0] - label_train[ijk,0])
         if(f_of_x_1[0,ijk_1,0] < 0 ):
             tmp3 = -1
         else:
             tmp3 = 1
-        # print(f_of_x[0,ijk,0] - label_train[ijk,0])
         if ((tmp3 == -1 and train_label_dataset[ijk_1,0] ==-1 ) or ((tmp3 == 0 or tmp3 == 1 ) and train_label_dataset[ijk_1,0] == 1)):
             correct_1 +=1
-    print("Acccuracy",100*(correct_1/len(train_dataset)))
+    print("Training Acccuracy",100*(correct_1/len(train_dataset)))
     return weights_final,central_neurons
-    # print("wrong",len(train_dataset)-correct_1)
+
 def test_pred(test_dataset,weights_final_from_train_,central_neurons,number_of_centres):
     distance_in_neurons = np.zeros([len(test_dataset),number_of_centres])
     PHI_2 = np.zeros([len(test_dataset),number_of_centres])
     d_max = 0
     for i_2 in range(len(test_dataset)):
         for i_3 in range(number_of_centres):
-            # distance_in_neurons[i_2,i_3] = distance.euclidean(test_dataset[i_2,:],central_neurons[i_3,:])
             d_distance = np.linalg.norm(test_dataset[i_2,:] - central_neurons[i_3,:])
             if (d_max<=d_distance):
                 d_max = d_distance
-            
             sigma = d_max/(number_of_centres**0.5)
             PHI_2[i_2,i_3] = radial_gaussian_function(d_distance,sigma)
-    # print("Max_distace",d_max)
-    # print("distance",distance_in_neurons)
+
     test_label_dataset_output = np.matmul(PHI_2,weights_final_from_train_)
     # print("test_label_predicition", test_label_dataset_output)
     # print("test_label_predicition",test_label_dataset_output.shape)
-    # np.where[test_label_dataset_output < 0 ,-1,1]
     test_label_dataset_output1 = test_label_dataset_output[0,:,:]
-    print("test_label_predicition1",test_label_dataset_output1.shape)
-    
+    print("test_label_predicition",test_label_dataset_output1.shape)
     pred = list()
     iem = 0
     for iem in range(len(test_label_dataset_output1)):
@@ -164,8 +101,37 @@ def test_pred(test_dataset,weights_final_from_train_,central_neurons,number_of_c
             pred.append(1)            
     # print("Prediction result ",pred)
     return pred
-centres = input("Enter the number of Central neurons to be considered ")
+def svm():
+    X_train = data_train
+    y_train = label_train
+    X_test = data_test
+    # y_test = np.transpose(label_test)
+    from sklearn.svm import SVC
+    svclassifier = SVC(kernel='rbf',gamma='auto')
+    svclassifier.fit(X_train, y_train)
+    #To use Gaussian kernel, you have to specify 'rbf' as value for the Kernel parameter of the SVC class.
+
+    #Prediction and Evaluation
+    y_pred = svclassifier.predict(X_test)
+    # y_train_pred = svclassifier.predict(X_train)
+    print("SVM Prediction",y_pred)
+    return y_pred
+import sys
+centres = int (sys.argv[1])
+prechoose = int (sys.argv[2])
+# centres = input("Enter the number of Central neurons to be considered ")
+print("Number of centres", centres)
 train_weights,trian_central_neurons = weight_calculation(data_train,label_train,int(centres))
 label_test_prediction = test_pred(data_test,train_weights,trian_central_neurons,int(centres))
 print ("Label_test",np.transpose(label_test_prediction))
-
+svm_pred = svm()
+i = 0
+correct = 0
+correct_1 = 0
+for i in range(len(svm_pred)):
+    if(svm_pred[i] ==  label_test_prediction[i]):
+        correct += 1
+    else:
+        correct += 0
+print("Considering the svm is the most coorect one")
+print("rbf is ",100*( correct/len(svm_pred)),"accuracy to SVM")
--- svm_using_gaussian_kernel.py
+++ svm_using_gaussian_kernel.py
@@ -1,6 +1,6 @@
 from enum import auto
 import numpy as np
-from numpy.core.numeric import correlate
+# from numpy.core.numeric import correlate
 ###import the scipy for loading .mat matlab data
 from scipy.io import loadmat
 ### loading the data in same name variable
@@ -23,18 +23,55 @@ print("data_test ",type(data_test),"_",data_test_row,data_test_column)
 print("data_train ",type(data_train),"_",data_train_row,data_train_column)
 print("label_train ",type(label_train),"_",label_train_row,label_train_column)
 
+label_test = np.array([[1, -1, 1, 1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, 1, 1, 1]])
+# i= 0
+# avg = np.zeros(len(data_test))
+# j = list()
+# for i in range(len(data_test)):
+#     avg = np.mean(data_test[i,:])
+#     if(avg>0):
+#         j.append(1)
+#     else:
+#         j.append(-1)
+# print(avg)
+# print(j)
+# print(data_test)
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# H = data_test[:,:]  # added some commas and array creation code
+
+# fig = plt.figure(figsize=(6, 3.2))
+
+# ax = fig.add_subplot(111)
+# ax.set_title('classic')
+# plt.imshow(H)
+# ax.set_aspect('equal')
+
+# cax = fig.add_axes([0.12, 0.1, 0.78, 0.8])
+# cax.get_xaxis().set_visible(False)
+# cax.get_yaxis().set_visible(False)
+# cax.patch.set_alpha(0)
+# cax.set_frame_on(False)
+# plt.colorbar(orientation='vertical')
+# plt.show()
 X_train = data_train
 y_train = label_train
-X_test = data_train
-y_test = label_train
+X_test = data_test
+y_test = np.transpose(label_test)
 from sklearn.svm import SVC
-svclassifier = SVC(kernel='rbf')
+svclassifier = SVC(kernel='rbf',gamma='auto')
 svclassifier.fit(X_train, y_train)
 #To use Gaussian kernel, you have to specify 'rbf' as value for the Kernel parameter of the SVC class.
 
 #Prediction and Evaluation
 y_pred = svclassifier.predict(X_test)
+# y_train_pred = svclassifier.predict(X_train)
+print(y_pred)
+i=0
+for i in range(len(y_pred)):
+    print(y_pred[i])
 from sklearn.metrics import classification_report, confusion_matrix
-print(confusion_matrix(y_test, y_pred))
-print(classification_report(y_test, y_pred))
+# print(confusion_matrix(y_test, y_pred))
+# print(classification_report(y_test, y_pred))
 #The output of the Kernel SVM with Gaussian kernel looks like this:
